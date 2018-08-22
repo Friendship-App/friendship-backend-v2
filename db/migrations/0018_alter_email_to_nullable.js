02da8acac2e0f88704ab5c9636a0bbba5dc1de3e
chengjo0 <john.cheng00@gmail.com>
2018-08-22 11:24:19 +0300

    Migration to change email to nullable done

---
+++ db/migrations/0018_alter_email_to_nullable.js
@@ -0,0 +1,11 @@
+exports.up = knex => {
+  return knex.schema.hasTable('users').then(exists => {
+    if (exists) {
+      return knex.schema.alterTable('users', t => {
+        t.text('email').alter();
+      });
+    }
+  });
+};
+
+exports.down = knex => knex.schema.table.dropTableIfExists('users');
