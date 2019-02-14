5f3970f9f67dfd2daee3ee855a61eb9f927806ea
Petri Huovinen <petri.huovinen@houston-inc.com>
2019-02-14 13:24:53 +0200

    Send push notification when message is sent

--- src/handlers/messages.js
+++ src/handlers/messages.js
@@ -3,7 +3,9 @@ import {
   dbGetMessages,
   dbRegisterNewUnreadMessage,
   dbUpdateMessages,
+  dbGetDataForMessageNotification,
 } from '../models/messages';
+import { notifyReveiceNewMessage } from '../utils/notifications';
 
 export const updateMessages = (request, reply) =>
   dbUpdateMessages(request.query.chatroomId, request.pre.user.id).then(
@@ -13,20 +15,31 @@ export const updateMessages = (request, reply) =>
 export const getMessages = (request, reply) =>
   dbGetMessages(request.query.chatroomId).then(data => reply.response(data));
 
+const sendNewMessagePushNotification = async (senderId, chatroomId) => {
+  const dataForMessageNotifications = await dbGetDataForMessageNotification(
+    senderId,
+    chatroomId,
+  );
+  notifyReveiceNewMessage(dataForMessageNotifications);
+};
+
 export const createMessage = async function(request, reply) {
+  const senderId = request.pre.user.id;
+  const chatroomId = request.payload.chatroomId;
   return dbCreateMessage({
     chatTime: new Date(),
-    senderId: request.pre.user.id,
+    senderId,
     textMessage: request.payload.textMessage,
-    chatroomId: request.payload.chatroomId,
+    chatroomId,
   })
     .then(async message => {
+      sendNewMessagePushNotification(senderId, chatroomId);
       const newUnreadMessages = [];
       request.payload.receiverId.map(id => {
         newUnreadMessages.push({
           messageId: message.id,
           receiverId: id,
-          chatroomId: message.chatroomId,
+          chatroomId,
         });
       });
       await dbRegisterNewUnreadMessage(newUnreadMessages);
